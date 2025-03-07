1bcb1a3d0250200848708f70db34f4b53b29ff6b
renatolucianosilva <renatolulucianosilva@gmail.com>
2025-03-07 18:54:29 -0300

    methods get findAllboks and savebook

---
+++ com.ebookmanager/src/main/java/com/ebookmanager/controller/LivroController.java
@@ -0,0 +1,53 @@
+package com.ebookmanager.controller;
+
+import com.ebookmanager.mapper.LivroMapper;
+import com.ebookmanager.model.Livro;
+import com.ebookmanager.request.LivroPostRequest;
+import com.ebookmanager.response.LivroGetResponse;
+import com.ebookmanager.response.LivroPostResponse;
+import com.ebookmanager.service.LivroService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("v1/livros")
+@AllArgsConstructor
+public class LivroController {
+
+    private final LivroService livroService;
+
+    private final LivroMapper Mapper;
+
+    @PostMapping
+    public ResponseEntity<LivroPostResponse> saveBook(@RequestBody LivroPostRequest livroRequest){
+
+        var livro = Mapper.livroPostRequestToLivro(livroRequest);
+
+        var livroSaved = livroService.save(livro);
+
+
+
+        var livroResponse = Mapper.livroPostResponseToLivro(livroSaved);
+
+        System.out.println(livroResponse.getId());
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(livroResponse);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<LivroGetResponse>> allBooks(){
+
+        var listLivros = livroService.findAll();
+
+        var listLivrosGetResponse = Mapper.listBooksToListBookGetResponse(listLivros);
+
+        return ResponseEntity.status(HttpStatus.OK).body(listLivrosGetResponse);
+
+
+    }
+
+}
