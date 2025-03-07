2ba31ce0dd0fb5dd3e7c5e14d66548d9716d6de6
renatolucianosilva <renatolulucianosilva@gmail.com>
2025-03-07 18:52:58 -0300

    class of getresponse

---
+++ com.ebookmanager/src/main/java/com/ebookmanager/response/LivroGetResponse.java
@@ -0,0 +1,17 @@
+package com.ebookmanager.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class LivroGetResponse {
+
+    private Long id;
+    private String titulo;
+    private String autor;
+    private Integer anoPublicacao;
+    private String genero;
+}
