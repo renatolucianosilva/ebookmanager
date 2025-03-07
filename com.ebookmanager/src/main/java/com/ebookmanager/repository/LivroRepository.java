585949eae194003ff576fea5bbe7fa7794099409
renatolucianosilva <renatolulucianosilva@gmail.com>
2025-03-07 18:55:11 -0300

    repository book

---
+++ com.ebookmanager/src/main/java/com/ebookmanager/repository/LivroRepository.java
@@ -0,0 +1,9 @@
+package com.ebookmanager.repository;
+
+import com.ebookmanager.model.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Long> {
+}
