c9e6963362590614d6d070027b2dacaeea0d4450
renatolucianosilva <renatolulucianosilva@gmail.com>
2025-03-07 14:32:30 -0300

    commit inicial

---
+++ com.ebookmanager/src/test/java/com/ebookmanager/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebookmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
