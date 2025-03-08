1415921d53d2513bd79d763cdb4839fe3e4f47a2
renatolucianosilva <renatolulucianosilva@gmail.com>
2025-03-08 17:23:24 -0300

    criação da entidade Usuario

---
+++ com.ebookmanager/src/main/java/com/ebookmanager/model/Usuario.java
@@ -0,0 +1,32 @@
+package com.ebookmanager.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_usuarios")
+
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_usuario")
+    private Long idUsuario;
+
+    @Column(name = "name")
+    private String nome;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "telefone")
+    private String telefone;
+
+    @Column(name="adplencia")
+    private Boolean adplencia;
+
+
+}
