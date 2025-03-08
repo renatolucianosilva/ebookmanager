7322bdb85a47f359bef0aa0ec894636104b11c47
renatolucianosilva <renatolulucianosilva@gmail.com>
2025-03-08 12:53:45 -0300

    Inteface Livro repository; pesquisa bytitulo

--- com.ebookmanager/src/main/java/com/ebookmanager/repository/LivroRepository.java
+++ com.ebookmanager/src/main/java/com/ebookmanager/repository/LivroRepository.java
@@ -1,9 +1,14 @@
 package com.ebookmanager.repository;
 
+import com.ebookmanager.exceptions.BadRequestException;
 import com.ebookmanager.model.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface LivroRepository extends JpaRepository<Livro, Long> {
+
+    List<Livro> findByTitulo(String titulo) throws BadRequestException;
 }
